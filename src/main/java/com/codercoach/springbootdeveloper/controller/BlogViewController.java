414a764a603678ceadd5cd75122253bac58c7895
akj2025 <akjakj2013@gmail.com>
2025-07-08 11:18:33 +0900

    헤더 푸터 사이드바 정리

--- src/main/java/com/codercoach/springbootdeveloper/controller/BlogViewController.java
+++ src/main/java/com/codercoach/springbootdeveloper/controller/BlogViewController.java
@@ -5,6 +5,7 @@ import com.codercoach.springbootdeveloper.dto.ArticleListViewResponse;
 import com.codercoach.springbootdeveloper.dto.ArticleViewResponse;
 import com.codercoach.springbootdeveloper.service.BlogService;
 import com.example.util.MarkdownUtil;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
@@ -23,11 +24,12 @@ public class BlogViewController {
 
     // 블로그 글 목록 조회
     @GetMapping(value = "/articles", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public String getArticles(Model model) {
+    public String getArticles(HttpServletRequest request, Model model) {
         List<ArticleListViewResponse> articles = blogService.findAll().stream()
                 .map(ArticleListViewResponse::new)
                 .toList();
         model.addAttribute("articles", articles); // 블로그 글 리스트 저장
+        model.addAttribute("currentUri", request.getRequestURI()); // 컨트롤러에서 현재 URI를 model로 넘기기
 
         return "articleList"; // articleList.html 뷰 조회
     }
--- src/main/java/com/codercoach/springbootdeveloper/dto/ArticleListViewResponse.java
+++ src/main/java/com/codercoach/springbootdeveloper/dto/ArticleListViewResponse.java
@@ -1,6 +1,7 @@
 package com.codercoach.springbootdeveloper.dto;
 
 import com.codercoach.springbootdeveloper.domain.Article;
+import com.example.util.MarkdownUtil;
 import lombok.Getter;
 
 @Getter
@@ -9,10 +10,12 @@ public class ArticleListViewResponse {
     private final Long id;
     private final String title;
     private final String content;
+    private final String contentHtml;
 
     public ArticleListViewResponse(Article article) {
         this.id = article.getId();
         this.title = article.getTitle();
         this.content = article.getContent();
+        this.contentHtml = MarkdownUtil.toHtml(article.getContent());
     }
 }
